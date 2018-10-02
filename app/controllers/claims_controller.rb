f0f8144172c5816c7b029985a6db4a06da1fd827
ainunonone <ainunrd_29@yahoo.com>
2018-10-02 10:56:02 +0800

    button design baru

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -2,15 +2,16 @@ class ClaimsController < ApplicationController
   def index
 	if params[:search]
 		query_array = []
-		if !params[:search][:month].blank?
-			query_array.push("strftime('%m', created) = '#{params[:search][:month]}'")
-		end
-		if !params[:search][:year].blank?
-			query_array.push("strftime('%Y' , created) = '#{params[:search][:year]}'")
-		end
-		@user_monthly_approvals = UserMonthlyApproval.where(query_array.join(" and "))
+		#if !params[:search][:month].blank?
+		#	query_array.push("strftime('%m', created) = '#{params[:search][:month]}'")
+		#end
+		#if !params[:search][:year].blank?
+			#query_array.push("strftime('%Y' , created) = '#{params[:search][:year]}'")
+		#end
+		#@user_monthly_approvals = UserMonthlyApproval.where(query_array.join(" and "))
 	else
 		@user_monthly_approvals = UserMonthlyApproval.all
 	end
-  end
+	end
+ 
 end
